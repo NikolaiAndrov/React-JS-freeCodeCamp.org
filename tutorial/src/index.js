743b37be0c7dfbdf067409c732ee9e0c059cbcc2
nikolaiandrov <nikolaiandrov@abv.bg>
2024-05-01 14:02:41 +0300

    added more lines in div

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,8 +3,13 @@ import ReactDom from "react-dom/client";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-function Greeting(){
-    return <h2>Hello World! This is my first component.</h2>
+function Greeting() {
+  return(
+  <div>
+    <h1>Hello World!</h1>
+    <h2>This is my first component</h2>
+    <h3>Happy to learn React JS</h3>
+  </div>);
 }
 
-root.render(<Greeting/>);
+root.render(<Greeting />);
