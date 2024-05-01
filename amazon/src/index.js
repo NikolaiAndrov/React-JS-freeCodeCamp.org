876d7b6d9fd6d43d0f8d2f1d977b79c54957f88d
nikolaiandrov <nikolaiandrov@abv.bg>
2024-05-01 17:09:24 +0300

    css added to the simple amazon best selling books project

--- amazon/src/index.js
+++ amazon/src/index.js
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
+import "./index.css"
+
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 const BookList = () => {
-  return <section>
-    <Book></Book>
-  </section>;
+  return (
+    <section className="booklist">
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+    </section>
+  );
 };
 
 const Book = () => {
-  return <article>
+  return <article className="book">
     <Image></Image>
     <Title></Title>
     <Author></Author>
   </article>;
 };
 
-const Image = () => <h2>image placeholder</h2>;
-const Title = () => <h2>Book Title</h2>;
-const Author = () => <h2>Book Author</h2>;
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/913C+MR3S5L._AC_UL900_SR900,600_.jpg"
+    alt="The Women"
+  />
+);
+
+const Title = () => <h2>The Women</h2>;
+const Author = () => <h4>Kristin Hannah</h4>;
 
 root.render(<BookList></BookList>);
