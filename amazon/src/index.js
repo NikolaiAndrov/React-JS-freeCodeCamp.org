44f5335e51b39613d6b4528c948ac108eed4323c
nikolaiandrov <nikolaiandrov@abv.bg>
2024-05-02 12:53:53 +0300

    displaying books through objects, img height 400px

--- amazon/src/index.js
+++ amazon/src/index.js
@@ -5,16 +5,30 @@ import "./index.css";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-const title = "The Women"; 
-const author = "Kristin Hannah";
-const img = "./images/image1.jpg";
+const firstBook = {
+  title: "The Women",
+  author: "Kristin Hannah",
+  img: "https://m.media-amazon.com/images/I/913C+MR3S5L._SL1500_.jpg",
+};
+
+const secondBook = {
+  title: "Mom, I Want to Hear Your Story",
+  author: "Jeffrey Mason",
+  img: "https://m.media-amazon.com/images/I/71sOqrd6JHL._SL1499_.jpg",
+};
+
+const thirdBook = {
+  title: "The Lost Book of Herbal Remedies",
+  author: "Claude Davis",
+  img: "https://m.media-amazon.com/images/I/71++zre30EL._SL1360_.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book title={title} author={author} img={img}></Book>
-      <Book title={title} author={author} img={img}></Book>
-      <Book title={title} author={author} img={img}></Book>
+      <Book title={firstBook.title} author={firstBook.author} img={firstBook.img}></Book>
+      <Book title={secondBook.title} author={secondBook.author} img={secondBook.img}></Book>
+      <Book title={thirdBook.title} author={thirdBook.author} img={thirdBook.img}></Book>
     </section>
   );
 };
