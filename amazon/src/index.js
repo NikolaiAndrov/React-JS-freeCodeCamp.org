776cb0b235667ee9c751a50ea94c48f70df9f8e8
nikolaiandrov <nikolaiandrov@abv.bg>
2024-05-03 14:25:22 +0300

    refactoring

--- amazon/src/index.js
+++ amazon/src/index.js
@@ -7,18 +7,21 @@ const root = ReactDom.createRoot(document.getElementById("root"));
 
 const books = [
   {
+    id: 1,
     title: "The Women",
     author: "Kristin Hannah",
     img: "https://m.media-amazon.com/images/I/913C+MR3S5L._SL1500_.jpg",
   },
 
   {
+    id: 2,
     title: "Mom, I Want to Hear Your Story",
     author: "Jeffrey Mason",
     img: "https://m.media-amazon.com/images/I/71sOqrd6JHL._SL1499_.jpg",
   },
 
   {
+    id: 3,
     title: "The Lost Book of Herbal Remedies",
     author: "Claude Davis",
     img: "https://m.media-amazon.com/images/I/71++zre30EL._SL1360_.jpg",
@@ -29,13 +32,14 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((b) => {
-        return <Book title={b.title} img={b.img} author={b.author} />;
+        return <Book {...b} key={b.key} />;
       })}
     </section>
   );
 };
 
-const Book = ({ title, img, author }) => {
+const Book = (props) => {
+  const {img, title, author} = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
