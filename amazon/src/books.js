849ade0db98f887948320849e2a0409f6ec3c74f
nikolaiandrov <nikolaiandrov@abv.bg>
2024-05-06 20:35:47 +0300

    books array exported in a file

---
+++ amazon/src/books.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    id: 1,
+    title: "The Women",
+    author: "Kristin Hannah",
+    img: "https://m.media-amazon.com/images/I/913C+MR3S5L._SL1500_.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Mom, I Want to Hear Your Story",
+    author: "Jeffrey Mason",
+    img: "https://m.media-amazon.com/images/I/71sOqrd6JHL._SL1499_.jpg",
+  },
+
+  {
+    id: 3,
+    title: "The Lost Book of Herbal Remedies",
+    author: "Claude Davis",
+    img: "https://m.media-amazon.com/images/I/71++zre30EL._SL1360_.jpg",
+  },
+];
--- amazon/src/index.js
+++ amazon/src/index.js
@@ -2,31 +2,9 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 import "./index.css";
+import { books } from "./books";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-
-const books = [
-  {
-    id: 1,
-    title: "The Women",
-    author: "Kristin Hannah",
-    img: "https://m.media-amazon.com/images/I/913C+MR3S5L._SL1500_.jpg",
-  },
-
-  {
-    id: 2,
-    title: "Mom, I Want to Hear Your Story",
-    author: "Jeffrey Mason",
-    img: "https://m.media-amazon.com/images/I/71sOqrd6JHL._SL1499_.jpg",
-  },
-
-  {
-    id: 3,
-    title: "The Lost Book of Herbal Remedies",
-    author: "Claude Davis",
-    img: "https://m.media-amazon.com/images/I/71++zre30EL._SL1360_.jpg",
-  },
-];
 
 const BookList = () => {
   return (
